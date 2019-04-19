bb19b2aae52a121c8aae4b176a8315e908c2d0bf
laulau <olaulau@gmail.com>
2019-04-19 16:37:48 +0200

    changed labels, date in full french format

--- priority.php
+++ priority.php
@@ -5,7 +5,7 @@ $f3 = \Base::instance();
 $db = new DB\SQL('sqlite:./api/taskboard.db');
 
 $sql = '
-	SELECT l.name AS colonne, title AS titre, description, due_date as délai
+	SELECT l.name AS étape, title AS titre, description, due_date as délai
 	FROM item i
 	INNER JOIN lane l ON i.lane_id = l.id';
 $data = $db->exec($sql);
@@ -14,27 +14,66 @@ $data2 = [];
 foreach ($data as $id => $row) {
 	if (!empty ($row['délai'])) {
 		$date = DateTime::createFromFormat('m/d/Y', $row['délai']);
-		$row['délai'] = $date->format('d/m/Y');
 		$real_date = $date->format('Y-m-d');
-		$data[$real_date] = $row;
-	}
-	unset($data[$id]);
-	
-	if (!empty ($row['délai'])) {
 		unset($row['délai']);
-	if (!isset ($data2[$real_date])) {
-		$data2[$real_date] = [];
-	}
-	$data2[$real_date][] = $row;
+    	if (!isset ($data2[$real_date])) {
+    		$data2[$real_date] = [];
+    	}
+    	$data2[$real_date][] = $row;
 	}
 }
-ksort($data);
 ksort($data2);
 
-$row = reset($data2);
-$row = reset($row);
-$titles = array_keys($row);
-$nb_titles = count ($titles);
+$months = [
+    'en' => [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'Jully',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ],
+    'fr' => [
+        'Janvier',
+        'Février',
+        'Mars',
+        'Avril',
+        'Mai',
+        'Juin',
+        'Juillet',
+        'Août',
+        'Septembre',
+        'Novembre',
+        'Décembre',
+    ]
+];
+
+$days_of_week = [
+    'en' => [
+        'Sunday',
+        'Monday',
+        'Thuesday',
+        'Wednesday',
+        'Thusday',
+        'Friday',
+        'Saturday',
+    ],
+    'fr' => [
+        'Dimanche',
+        'Lundi',
+        'Mardi',
+        'Mercredi',
+        'Jeudi',
+        'Vendredi',
+        'Samedi',
+    ]
+];
 
 ?>
 
@@ -48,36 +87,30 @@ $nb_titles = count ($titles);
 		<table class="table table-bordered">
 			<thead>
 			<tr>
-				<?php
-				foreach ($titles as $title) {
-					?>
-					<th><?= $title ?></th>
-					<?php
-				}
-				?>
+				<th>étape</th>
+				<th>titre</th>
+				<th>description</th>
 			</tr>
 			</thead>
 			<?php
 			foreach ($data2 as $date => $data) {
+			    $date = DateTime::createFromFormat('Y-m-d', $date);
+			    $date = $date->format('l d F Y');
+			    $date = str_replace($months['en'], $months['fr'], $date);
+			    $date = str_replace($days_of_week['en'], $days_of_week['fr'], $date);
 				?>
-				<tr><td colspan="<?= $nb_titles ?>"><h3><?= $date ?></h3></td></tr>
+				<tr><td colspan="3"><h3><?= $date ?></h3></td></tr>
 				<?php
-				
 				foreach ($data as $row) {
 				?>
 				<tr>
-					<?php
-					foreach ($row as $col) {
-						?>
-						<td><?= $col ?></td>
-						<?php
-					}
-					?>
+					<td><?= $row['étape'] ?></td>
+					<td><?= $row['titre'] ?></td>
+					<td><?= $row['description'] ?></td>
 				</tr>
 				<?php
 				}
 			}
-			
 			
 			?>
 		</table>
